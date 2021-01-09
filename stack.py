3905da1b87c51a17f096ce1dea376e0155a050b3
lzgrshkv <74429330+lzgrshkv@users.noreply.github.com>
2021-01-09 17:31:28 +0300

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,33 @@
+class EmptyStackException(Exception):
+  pass
+  
+class Stack:
+  class Node:
+    def __init__(self, value, nextNode):
+      self.value = value
+      self.nextNode = nextNode
+      
+  def __init__(self):
+    self.top = None
+    
+  def isEmpty(self):
+    return self.top is None
+    
+  def push(self, value):
+    self.top = Stack.Node(value, self.top)
+    
+  def pop(self):
+    if self.isEmpty():
+      raise EmptyStackException
+    val = self.top.value
+    self.top = self.top.nextNode
+    return val
+    
+if __name__ == "__main__":
+  s = Stack(1)
+  s.push(1)
+  s.push(2)
+  s.push(3)
+  
+  while not s.isEmpty():
+    print(s.pop())
